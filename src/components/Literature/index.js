979b91305571505cb59b15ae0a4451cea7338dc7
Courtney <courtneyeott@gmail.com>
2019-04-15 11:28:22 -0600

    make articles show up on lit page

--- src/components/Literature/index.js
+++ src/components/Literature/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class LitPage extends Component {
-  render(){
-    return(
+  state = {
+    dreamArticles: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=dreams&api-key=OQKttP42ZWiOZdLWaBXQ1nfvbUKkU4Hb`)
+      .then(res => {
+        const dreamArticles = res.data.response.docs;
+        console.log(dreamArticles);
+        this.setState({ dreamArticles });
+      })
+  }
+
+  render() {
+    return (
       <div>
-        <h1>Explore</h1>
+      {this.state.dreamArticles.map(article => <p>{article.headline.main}</p>)}
       </div>
     )
   }
-
 }
 
+
+
 export default LitPage
+
+
+
+
+
